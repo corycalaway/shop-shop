be59997838cb5317b4130cfb807e427f6340b0f8
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-13 17:40:24 -0600

    setup global store

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+// With this function, StoreProvider, we instantiate our initial global state with the useProductReducer() function we created earlier. Because that wraps it around the useReducer() Hook from React, every time we run this useProductReducer() function, we receive the following two items in return:
+
+// state is the most up-to-date version of our global state object.
+
+// dispatch is the method we execute to update our state. It is specifically going to look for an action object passed in as its argument, as we'll soon see.
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
